347852e2715c66194d140b3281e0ab8931e0f4b9
ttoullie <t.toullier@gmail.com>
2017-03-01 15:29:25 +0100

    Copyright added

--- FigExporter.m
+++ FigExporter.m
@@ -1,3 +1,29 @@
+% Copyright 2016, 2017 Thibaud Toullier
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <http://www.gnu.org/licenses/>
+%
+% ----------------------------------------------------------------------------
+%% Usage
+% Figure Exporter is a simple interface that helps you for
+% formatting and exporting your plots in a simple way. 
+%
+% 1. 	First you need to download and install the GUI Layout Toolbox at
+% 	https://www.mathworks.com/matlabcentral/fileexchange/27758-gui-layout-toolbox
+%
+% 2.	Then you can execute the /testme.m/ file in Matlab to test
+% 	the application. and see how it works
+%
+
 classdef FigExporter < handle
     properties
         version = '0.1'
