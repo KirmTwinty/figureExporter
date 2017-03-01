50553c4ef6211fe8a9ba60a4b361fa53a775d1a9
ttoullie <t.toullier@gmail.com>
2017-03-01 14:55:41 +0100

    Initial Commit - v0.1

--- testme.m
+++ testme.m
@@ -1,7 +1,7 @@
 close all;
 clear all;
 clc;
-addpath('../../../Code/Infrared_Image_Processing_Tool/iipt/include/layout/');
+addpath(); % Path to the layout toolbox
 
 [X,Y] = meshgrid(-8:.5:8);
 R = sqrt(X.^2 + Y.^2) + eps;
